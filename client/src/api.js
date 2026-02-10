d88bb55633aacaa10cc62d19191717bbb23dcc13
Chloe-ntc <chloenitcheu24@gmail.com>
2026-02-10 02:13:29 +0100

    Update API URL to deployed backend

--- client/src/api.js
+++ client/src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Change baseURL depending on environment
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: "https://mern-portfolio-3-h52l.onrender.com/",
 });
